072e0b8f0d822020dbe433ea5a8b97da3686a4df
Enes Eren Seven <eneseren2526@gmail.com>
2025-03-19 10:22:46 +0300

    tasarım şablonu yazırlandı

---
+++ deneme/resources/views/admin/sabit/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-sm-6">
+                               Dpyazilim.com.tr © Upcube.
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="text-sm-end d-none d-sm-block">
+                                   Poyraz Dik ile <i class="mdi mdi-heart text-danger"></i> oluşturuldu.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
